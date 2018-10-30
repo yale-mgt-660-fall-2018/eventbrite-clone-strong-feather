ba32a836fcc3ce35a60f7dc951c6bbc600256463
Race Wright <rwright@uchicago.edu>
2018-10-30 17:25:58 -0400

    Adding a default fake DB URL
    Adding a default fake URL for the DB configuration. This seems to solve the problem I had launching the default app.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    databaseURL: process.env.DATABASE_URL,
+    databaseURL: process.env.DATABASE_URL || 'https://example.com',
+    /*TODO(racewright): change fake URL to real URL.*/
 };
