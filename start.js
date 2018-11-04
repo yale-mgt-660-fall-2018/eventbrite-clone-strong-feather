fd56f5d8bc3ba279cb54024fa4997f1140a728e6
Ashish Rathi <35428407+ashishrathi123@users.noreply.github.com>
2018-11-04 15:06:14 -0500

    Rectifying error in start.js

--- start.js
+++ start.js
@@ -1,6 +1,5 @@
 const config = require('./src/config.js');
 const app = require('./src/app.js')(config);
-'use strict';
 
 app.listen(app.context.port, () => {
     console.log(`Server started on port ${app.context.port}`);
