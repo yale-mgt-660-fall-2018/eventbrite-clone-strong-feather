584370ff5e6b11ce019a1ae5008ea82ee6787329
Ashish Rathi <35428407+ashishrathi123@users.noreply.github.com>
2018-11-04 15:10:56 -0500

    Revert "Hello World"
    This reverts commit 91758dddd2d1ff233f93e2bf56663624c6fc437c.

--- start.js
+++ start.js
@@ -2,4 +2,9 @@ const config = require('./src/config.js');
 const app = require('./src/app.js')(config);
 
 app.listen(app.context.port, () => {
+<<<<<<< HEAD
     console.log(`Server started on port ${app.context.port}`);
+=======
+    console.log(`Server started on port ${app.context.port}`);
+});
+>>>>>>> parent of 91758dd... Hello World
