54d9016f8163ea6eb82f52f623da8c72b1ffd18a
Ashish Rathi <35428407+ashishrathi123@users.noreply.github.com>
2018-11-04 15:13:07 -0500

    Correcting start.js file

--- start.js
+++ start.js
@@ -1,10 +1,6 @@
 const config = require('./src/config.js');
 const app = require('./src/app.js')(config);
 
-app.listen(app.context.port, () => {
-<<<<<<< HEAD
-    console.log(`Server started on port ${app.context.port}`);
-=======
+ app.listen(app.context.port, () => {
     console.log(`Server started on port ${app.context.port}`);
 });
->>>>>>> parent of 91758dd... Hello World
