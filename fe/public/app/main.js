e8e559e0d08943e829b295c64c4dde4b006fa152
racewright <rwright@uchicago.edu>
2018-11-10 18:18:31 -0500

    initial create event modal commit

--- fe/public/app/main.js
+++ fe/public/app/main.js
@@ -9,13 +9,42 @@ main.config(['$interpolateProvider', function($interpolateProvider) {
   $interpolateProvider.endSymbol(']]');
 }]);
 
-Controllers.main = function($scope) {
+Controllers.main = function($scope, $mdDialog) {
   var ctrl = this;
 
   ctrl.events = [
     {'title': 'Mega Party', 'subtitle': 'Things we will not do.', 'img': 'https://i.ytimg.com/vi/8XUETU0Yrpo/hqdefault.jpg'},
     {'title': 'Huge Party', 'subtitle': 'Things we do.'},
   ];
+
+  ctrl.showModal = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: '../public/components/event/create.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose: true,
+      fullscreen: true,
+    }).then(function(answer) {
+      alert('Completed Modal');
+    }, function() {
+      alert('Canceled Modal');
+    });
+  }
+
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
 
 };
 
