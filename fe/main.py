82686056dd6a6e25fac8e6724903c694358c818b
Ashish Rathi <ashish.rathi123@gmail.com>
2018-12-09 22:13:54 -0500

    Changed database and created Read/write methods

--- fe/main.py
+++ fe/main.py
@@ -5,61 +5,57 @@ import jinja2
 import wsgiref.handlers
 import os
 import json
+import sys
+import MySQLdb
 from datetime import datetime
+#from flask import Flask
+#import psycopg2.pool
 
-#db_user = os.environ.get('CLOUD_SQL_USERNAME')
-#db_password = os.environ.get('CLOUD_SQL_PASSWORD')
-#db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
-#db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
-#
-#cnx = psycopg2.connect(dbname=db_name, user=db_user,
-#                       password=db_password, host=host)
+CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+CLOUDSQL_DATABASE_NAME = os.environ.get('CLOUDSQL_DATABASE_NAME')
 
-from main_postgres import db_name, db_user, db_password, host
+def connect_to_cloudsql():
+    if True:
+		try:
+			cloudsql_unix_socket = os.path.join('/cloudsql', CLOUDSQL_CONNECTION_NAME)
+			db = MySQLdb.connect(
+	            unix_socket=cloudsql_unix_socket,
+	            user=CLOUDSQL_USER,
+	            passwd=CLOUDSQL_PASSWORD,
+	            db=CLOUDSQL_DATABASE_NAME)
+	            
+		except Exception as e:
+			global error_message 
+			error_message = sys.exc_info()
+    else:
+        db = MySQLdb.connect(host='127.0.0.1', user='', passwd='', db='msqldb')
+    return db
+
+
+def executeQuery(query):
+	db = connect_to_cloudsql()
+	try:
+		cursor = db.cursor()
+		cursor.execute(query)
+		db.commit()
+		global response 
+		response = cursor.fetchall()
+		return response 
+	except Exception as e:
+		global error_message 
+		error_message = sys.exc_info()
+	return ""
+
 
 def format_to_json(data_stream):
   # NB. Dates don't do well in Python JSON.
   return json.dumps(data_stream, default=str)
 
 def get_data(parameter):
-  ## Return a constant for the moment.
-  return format_to_json(
-    [
-      {
-          'id': 0,
-          'title': 'SOM House Party',
-          'date': datetime(2018, 1, 17, 16, 30, 0),
-          'imageURL': 'http://i.imgur.com/pXjrQ.gif',
-          'location': 'Aaron \'s house',
-          'attending': ['kyle.jensen@yale.edu', 'kim.kardashian@yale.edu'],
-      },
-      {
-          'id': 1,
-          'title': 'BBQ party for hackers and nerds',
-          'date': datetime(2017, 8, 1, 19, 0, 0),
-          'imageURL': 'http://i.imgur.com/7pe2k.gif',
-          'location': 'Sharon Oster\'s house',
-          'attending': ['kyle.jensen@yale.edu', 'kim.kardashian@yale.edu'],
-      },
-      {
-          'id': 2,
-          'title': 'BBQ for managers',
-          'date': datetime(2017, 12, 20, 18, 0, 0),
-          'imageURL': 'http://i.imgur.com/CJLrRqh.gif',
-          'location': 'Barry Nalebuff\'s house',
-          'attending': ['kim.kardashian@yale.edu'],
-      },
-      {
-          'id': 3,
-          'title': 'Cooking lessons for the busy business student',
-          'date': datetime(2018, 3, 2, 19, 0, 0),
-          'imageURL': 'http://i.imgur.com/02KT9.gif',
-          'location': 'Yale Farm',
-          'attending': ['homer.simpson@yale.edu'],
-      },
-    ]
-
-  )
+	## Return a constant for the moment.
+	return format_to_json(getEventDetails())
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -71,8 +67,14 @@ class MainPageHandler(webapp.RequestHandler):
   def get(self):
 
     print self.request
+    # TODO remove before submission. 5 lines of Aarons Testing code. Replace value of errormessage in template_values to ''
+    #createEvent(None)
+    #deleteEvent(None)
+    #addAttendee(None)
+    #addDonation(None)
+    #eventDetails = getEventDetails()
 
-    template_values = {'title': 'Strong Feather Events'}
+    template_values = {'title': 'Strong Feather Events', 'errormessage': ''}
     template = JINJA_ENVIRONMENT.get_template('index.html')
 
     self.response.write(template.render(template_values))
@@ -104,45 +106,77 @@ app = webapp.WSGIApplication(
   ],
   debug=os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/')
 )
-'''
+
 # Get all event details
 def getEventDetails():
-	events = executeQuery("SELECT * FROM events;")
-	attendees = executeQuery("SELECT * from attendees WHERE eventid IN (SELECT eventid FROM events) AND status = \"Yes\"")
+	#global allevents
+	allevents = executeQuery("SELECT eventid, eventname, eventtime, imagelink, location, duration FROM events;") 
+	attendees = executeQuery("SELECT eventid, useremail from attendees WHERE eventid IN (SELECT eventid FROM events) AND status = \"Yes\";")
+	#allevents = executeQuery("SELECT e.*, (SELECT GROUP_CONCAT(a.useremail SEPARATOR ', ') FROM attendees a WHERE a.eventid = e.eventid) AS attending FROM events e;")
+	eventsCopy = list()
+	for event in allevents:
+		newEvent = {}
+		newEvent['id'] = event[0]
+		newEvent['title'] = event[1]
+		newEvent['date'] = event[2]
+		newEvent['imageURL'] = event[3]
+		newEvent['location'] = event[4]
+		newEvent['attending'] = []
+		eventsCopy.append(newEvent)
+	for attendee in attendees:
+		for newEvent in eventsCopy:
+			if attendee[0] == newEvent["id"]:
+				newEvent["attending"].append(attendee[1])
+				break
+	return eventsCopy
 
 # Create new event
 def createEvent(eventObject):
-	# Dummy object
+	# TODO: Remove hardcode
+	# @Race: This is the object format
 	eventObject = {
-            'eventname' : "Party at Race\'s",
-            'location' : '',
-            'eventtime' : "No courses to display at this time.",
-            'duration_mins' : '',
-            'imagelink' : ''
+            'title' : "Party at Aarons",
+            'location' : "Races house",
+            'date' : "2019-01-01 00:00:01",
+            'duration' : 120,
+            'imageURL' : "https://i.imgur.com/n3PQl9u.png"
             }
-
-
-INSERT INTO events (eventname, location, eventtime, duration_mins, imagelink) VALUES ('Party at Race''s', 'Races house', '2019-01-01 00:00:01', 120, 'https://i.imgur.com/n3PQl9u.png');
-
-# Delete an event
-DELETE FROM events WHERE eventid = :eventid, eventid = eventid
+	if "duration" not in eventObject:
+		newEvent['duration'] = ""
+	response = executeQuery("INSERT INTO events (eventname, location, eventtime, duration, imagelink) VALUES (\'" + eventObject["title"] + "\', \'" + eventObject["location"] + "\', \'" + eventObject["date"] + "\', \'" + str(eventObject["duration"]) + "\', \'" + eventObject["imageURL"] + "\');")
+	return response
+	
+# Delete an event by eventid
+def deleteEvent(eventid):
+	#TODO Aaron Remove hardcode
+	eventid = 60
+	return executeQuery("DELETE FROM events WHERE eventid = " + str(eventid) + ";")
 
 # Add attendee to event
-INSERT INTO attendees (eventid, userid, status, registertime)
-VALUES (420, "aaron.dsouza@yale.edu", "Yes", NOW())
-
+def addAttendee(attendeeObject):
+	# @Race: This is the object format.
+	# TODO Aaron remove hardcode
+	attendeeObject = {
+		'eventid' : 60,
+		'useremail' : "aaron.dsouza@yale.edu",
+		'status' : "Yes"
+		}
+	# End of hardcode
+	response = executeQuery("INSERT INTO attendees (eventid, useremail, status, registertime) VALUES (" + str(attendeeObject['eventid']) + ", \'" + attendeeObject['useremail'] + "\', \'" + attendeeObject['status'] + "\', \'" + str(datetime.now()) + "\');")
+	return response
+	
+# TODO Aaron
 # Add donation to event
-INSERT INTO donations (eventid, userid, amount, timeofdonation)
-VALUES (420, "aaron.dsouza@yale.edu", 10.00, NOW())
-
-def executeQuery(query):
-	cnx = psycopg2.connect(dbname=db_name, user=db_user,
-                           password=db_password, host=host)
-	with cnx.cursor() as cursor:
-        cursor.execute(query)
-        result = cursor.fetchall()
-    current_time = result[0][0]
-    cnx.commit()
-    cnx.close()
-    return result
- '''
+def addDonation(donationObject):
+	# @Race: This is the object format.
+	# TODO Aaron remove hardcode
+	donationObject = {
+		'eventid' : 60,
+		'useremail' : "aaron.dsouza@yale.edu",
+		'amount' : 10.1
+		}
+	# End of hardcode
+	response = executeQuery("INSERT INTO donations (eventid, useremail, amount) VALUES (" + str(donationObject['eventid']) + ", \'" + donationObject['useremail'] + "\', " + str(donationObject['amount']) + ");")
+	return response
+	#INSERT INTO donations (eventid, userid, amount, timeofdonation) 
+	#VALUES (420, "aaron.dsouza@yale.edu", 10.00, NOW())
